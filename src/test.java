73d16bdd6d07d1cbefdfacb85465fcbbe59f590c
noomqwe0055 <30823963+noomqwe0055@users.noreply.github.com>
2017-08-22 15:01:32 +0700

    dd

--- src/test.java
+++ src/test.java
@@ -1,12 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Friend a1 = new Kullanat();
-		System.out.print(a1.getDescription() + " " + a1.Name() );
 		
+		
+		ArrayList<String> list1 = new ArrayList<String>(); 
+		
+		list1.add("Kullanat");
+		list1.add("Sittichok");
+		list1.add("Pattamaporn");
+		
+		for(String list:list1){
+			System.out.println(list);
+		}
+		
+		Collections.sort(list1);
+		
+		for(String list:list1){
+			System.out.println(list);
+		}
 
 	}
 
